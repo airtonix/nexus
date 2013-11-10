dd38eeaa9bcc7218c8040dbd039b9b9ee88f0eee
Zenobius Jiricek <airtonix@gmail.com>
2013-11-10 20:00:25 +1030

    remove reference to non exitant admin_media tag

--- nexus/templatetags/nexus_admin.py
+++ nexus/templatetags/nexus_admin.py
@@ -2,23 +2,24 @@ from django import template
 
 register = template.Library()
 
+
 def submit_row(context):
     """
-    Displays the row of buttons for delete and save. 
+    Displays the row of buttons for delete and save.
     """
-    opts = context['opts']
-    change = context['change']
-    is_popup = context['is_popup']
-    save_as = context['save_as']
+    opts = context.get('opts', False)
+    change = context.get('change', False)
+    is_popup = context.get('is_popup', False)
+    save_as = context.get('save_as', False)
     return {
         'onclick_attrib': (opts.get_ordered_objects() and change
-                            and 'onclick="submitOrderForm();"' or ''),
-        'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+                           and 'onclick="submitOrderForm();"' or ''),
+        'show_delete_link': (not is_popup and context.get('has_delete_permission', False)
+                             and (change or context.get('show_delete', False))),
         'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': context['has_add_permission'] and 
-                            not is_popup and (not save_as or context['add']),
-        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'show_save_and_add_another': context.get('has_add_permission', False) and
+                                     not is_popup and (not save_as or context.get('add', False)),
+        'show_save_and_continue': not is_popup and context.get('has_change_permission', False),
         'is_popup': is_popup,
         'show_save': True,
     }
